81b5bf2e81048e764d9522ca9e55b93a6e255fe2
ictye <northgreen2006@qq.com>
2023-10-12 22:55:36 +0800

    初步实现功能

--- bilibili_dm_plugin/__init__.py
+++ bilibili_dm_plugin/__init__.py
@@ -9,16 +9,71 @@
 #    版权所有者概不负责。如需技术支持，请联系版权所有者或社区获取最新版本。
 #
 #   更多详情请参阅许可协议文档
+try_imp = True
+import sys
+
+sys.path.append("./")
 
 import json
-
+from . import blivedm
 import plugin_main
 import msgs
 import asyncio
 import random
+import aiohttp
+from typing import *
+import http.cookies
+from .blivedm.clients import ws_base
+from .blivedm.models import web as web_models
+
+session: Optional[aiohttp.ClientSession] = None
+SESSDATA = ''
 
 
+# noinspection DuplicatedCode
 class Plugin_Main(plugin_main.Plugin_Main):
+    class Handler(blivedm.BaseHandler):
+        def __init__(self, cla: plugin_main.Plugin_Main):
+            self.main = cla
+            self.user_face: dict = {}
+
+        def _on_danmaku(self, client: blivedm.BLiveClient, message):
+            print(f'[{client.room_id}] {message.uname}：{message.msg}')
+            self.user_face[message.uname] = message.face
+            peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
+            message = msgs.msg_box(
+                message_class="default",
+                message_body=msgs.dm(
+                    msg=message.msg,
+                    who=msgs.msg_who(
+                        type=peop_type[message.privilege_type] if message.admin == 0 else 5,
+                        name=message.uname,
+                        face=message.face
+                    ).to_dict()
+                ).to_dict(),
+                msg_type="dm"
+            ).to_dict()
+            self.main.message_list.append(message)
+
+        def _on_gift(self, client: ws_base.WebSocketClientBase, message: web_models.GiftMessage):
+            print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+                  f' （{message.coin_type}瓜子x{message.total_coin}）')
+            peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
+            message = msgs.msg_box(
+                message_class="default",
+                message_body=msgs.info(
+                    msg=f"感谢{message.uname}赠送的{message.gift_name}",
+                    who=msgs.msg_who(
+                        type=peop_type[message.guard_level],
+                        name=message.uname,
+                        face=message.face
+                    ).to_dict(),
+                    pic=self.user_face[message.uname] if message.uname in self.user_face else "null"
+                ).to_dict(),
+                msg_type="info"
+            ).to_dict()
+            self.main.message_list.append(message)
+
     def plugin_init(self):
         self.sprit_cgi_support = True
         self.sprit_cgi_path = "test"
@@ -28,38 +83,19 @@ class Plugin_Main(plugin_main.Plugin_Main):
         return self.web.Response(text="ok")
 
     async def plugin_main(self):
-        def create_user():
-            usrname_list = ["guest1", "guest2", "guest3"]
-            usericon_list = ["ico1", "ico2", "ico3"]
-            return msgs.msg_who(random.randint(0, 6), usrname_list[random.randint(0, 2)],
-                                usericon_list[random.randint(0, 2)]).to_dict()
+        cookies = http.cookies.SimpleCookie()
+        cookies['SESSDATA'] = SESSDATA
+        cookies['SESSDATA']['domain'] = 'bilibili.com'
 
-        def create_dm():
-            message_list = ["一转九五三六", "好哎", "挖，主播好卡哇伊", "恭喜恭喜"]
-            return msgs.dm(random.choice(message_list),
-                           create_user()).to_dict()
+        session = aiohttp.ClientSession()
+        session.cookie_jar.update_cookies(cookies)
 
-        def create_info():
-            message_list = ["一转九五三六", "好哎", "挖，主播好卡哇伊", "恭喜恭喜"]
-            pic = ["a", "b", "c"]
-            return msgs.info(random.choice(message_list),
-                             create_user(),
-                             msgs.pic(random.choice([True, False]),
-                                      random.choice(pic)).to_dict()
-                             ).to_dict()
+        client = blivedm.BLiveClient(5050, session=session)
 
-        loop = 1000
-        while not loop == 0:
-            loop = loop - 1
-            print("已经产生一个消息")
-            if random.choice([True, False]):
-                print("msg")
-                msg = msgs.msg_box("default", "dm", create_dm()).to_dict()
-            else:
-                print("info")
-                msg = msgs.msg_box("default", "info", create_info()).to_dict()
-            self.message_list.append(msg)
-            await asyncio.sleep(3)
+        handler = self.Handler(self)
+        client.set_handler(handler)
+        client.start()
+        await client.join()
 
     def plugin_callback(self):
         print(f"plugin {__name__} is done")
