ca1739c4fc975eab2901aa76388fe92775fd9b23
ictye <95436661+northgreen@users.noreply.github.com>
2023-11-28 21:02:00 +0800

    Dev (#1)
    * 完整实现功能
    
    * 完整实现功能
    
    * 完整实现功能

--- bilibili_dm_plugin/__init__.py
+++ bilibili_dm_plugin/__init__.py
@@ -9,13 +9,16 @@
 #    版权所有者概不负责。如需技术支持，请联系版权所有者或社区获取最新版本。
 #
 #   更多详情请参阅许可协议文档
+import asyncio
+
 try_imp = True
 import sys
 
 sys.path.append("./")
 
 from . import blivedm
-from depends import plugin_main,msg
+from depends import plugin_main, msgs, connects
+
 import aiohttp
 from typing import *
 import http.cookies
@@ -26,51 +29,55 @@ session: Optional[aiohttp.ClientSession] = None
 SESSDATA = ''
 
 
+class Handler(blivedm.BaseHandler):
+    def __init__(self, lists: list):
+        self.user_face: dict = {}
+        self.lists = lists
+
+    def _on_danmaku(self, client: blivedm.BLiveClient, message):
+        print(f'[{client.room_id}] {message.uname}：{message.msg}')
+        self.user_face[message.uname] = message.face
+        peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
+        message = msgs.msg_box(
+            message_class="default",
+            message_body=msgs.dm(
+                msg=message.msg,
+                who=msgs.msg_who(
+                    type=peop_type[message.privilege_type] if message.admin == 0 else 5,
+                    name=message.uname,
+                    face=message.face
+                ).to_dict()
+            ).to_dict(),
+            msg_type="dm"
+        ).to_dict()
+        self.lists.append(message)
+
+    def _on_gift(self, client: ws_base.WebSocketClientBase, message: web_models.GiftMessage):
+        print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
+              f' （{message.coin_type}瓜子x{message.total_coin}）')
+        peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
+        message = msgs.msg_box(
+            message_class="default",
+            message_body=msgs.info(
+                msg=f"感谢{message.uname}赠送的{message.gift_name}",
+                who=msgs.msg_who(
+                    type=peop_type[message.guard_level],
+                    name=message.uname,
+                    face=message.face
+                ).to_dict(),
+                pic=self.user_face[message.uname] if message.uname in self.user_face else "null"
+            ).to_dict(),
+            msg_type="info"
+        ).to_dict()
+        self.lists.append(message)
+
+
 # noinspection DuplicatedCode
 class Plugin_Main(plugin_main.Plugin_Main):
-    class Handler(blivedm.BaseHandler):
-        def __init__(self, cla: plugin_main.Plugin_Main):
-            self.main = cla
-            self.user_face: dict = {}
-
-        def _on_danmaku(self, client: blivedm.BLiveClient, message):
-            print(f'[{client.room_id}] {message.uname}：{message.msg}')
-            self.user_face[message.uname] = message.face
-            peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
-            message = msgs.msg_box(
-                message_class="default",
-                message_body=msgs.dm(
-                    msg=message.msg,
-                    who=msgs.msg_who(
-                        type=peop_type[message.privilege_type] if message.admin == 0 else 5,
-                        name=message.uname,
-                        face=message.face
-                    ).to_dict()
-                ).to_dict(),
-                msg_type="dm"
-            ).to_dict()
-            self.main.message_list.append(message)
-
-        def _on_gift(self, client: ws_base.WebSocketClientBase, message: web_models.GiftMessage):
-            print(f'[{client.room_id}] {message.uname} 赠送{message.gift_name}x{message.num}'
-                  f' （{message.coin_type}瓜子x{message.total_coin}）')
-            peop_type = {0: 0, 1: 1, 2: 2, 3: 3}
-            message = msgs.msg_box(
-                message_class="default",
-                message_body=msgs.info(
-                    msg=f"感谢{message.uname}赠送的{message.gift_name}",
-                    who=msgs.msg_who(
-                        type=peop_type[message.guard_level],
-                        name=message.uname,
-                        face=message.face
-                    ).to_dict(),
-                    pic=self.user_face[message.uname] if message.uname in self.user_face else "null"
-                ).to_dict(),
-                msg_type="info"
-            ).to_dict()
-            self.main.message_list.append(message)
 
     def plugin_init(self):
+        self.runners = []
+
         self.sprit_cgi_support = True
         self.sprit_cgi_path = "test"
         return "message"
@@ -79,19 +86,37 @@ class Plugin_Main(plugin_main.Plugin_Main):
         return self.web.Response(text="ok")
 
     async def plugin_main(self):
-        cookies = http.cookies.SimpleCookie()
-        cookies['SESSDATA'] = SESSDATA
-        cookies['SESSDATA']['domain'] = 'bilibili.com'
-
-        session = aiohttp.ClientSession()
-        session.cookie_jar.update_cookies(cookies)
-
-        client = blivedm.BLiveClient(5050, session=session)
-
-        handler = self.Handler(self)
-        client.set_handler(handler)
-        client.start()
-        await client.join()
+        while True:
+            await asyncio.sleep(1)
 
     def plugin_callback(self):
         print(f"plugin {__name__} is done")
+
+    def dm_iter(self, params: dict, connect_waper: connects.connect_wrapper) -> object:
+        class dm_iter_back:
+            def __init__(self, params, connect_waper):
+                self.messages = []
+
+                cookies = http.cookies.SimpleCookie()
+                cookies['SESSDATA'] = SESSDATA
+                cookies['SESSDATA']['domain'] = 'bilibili.com'
+
+                session = aiohttp.ClientSession()
+                session.cookie_jar.update_cookies(cookies)
+
+                self.client = blivedm.BLiveClient(params["broom"], session=session)
+
+                handler = Handler(self.messages)
+                self.client.set_handler(handler)
+                self.client.start()
+
+            async def __aiter__(self):
+                try:
+                    yield self.messages.pop()
+                except IndexError:
+                    return
+
+            def __del__(self):
+                asyncio.get_event_loop().create_task(self.client.stop_and_close())
+
+        return dm_iter_back(params, connect_waper)
